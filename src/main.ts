a027688a93775eeb1587f0719cf134e864a645d3
Владислав Трегубов <valdiszz53@gmail.com>
2026-05-03 08:38:51 +0300

    Refactor HTML structure and styles for subscription form; implement form submission handling in TypeScript

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,25 @@
 import './reset.css'
 import './style.scss'
 
-//
+const container = document.querySelector<HTMLDivElement>('#container')!
+const form = document.querySelector<HTMLFormElement>('#form')!
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+
+    const formData = new FormData(form)
+
+    const email = formData.get('email') as string
+
+    container.setAttribute('data-state', 'success')
+
+    const successEmailEl =
+        document.querySelector<HTMLSpanElement>('#success-email')!
+
+    successEmailEl.textContent = email
+})
+
+const dismissButton = document.querySelector<HTMLButtonElement>('#dismiss')!
+dismissButton.addEventListener('click', () => {
+    container.removeAttribute('data-state')
+})
